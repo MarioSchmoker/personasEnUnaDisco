545f512f68f3904bc0e174d222837a62e20d78bd
Mario Schmoker <103613451+MarioSchmoker@users.noreply.github.com>
2022-06-02 18:29:29 -0300

    corregido

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 let min: number = 18;
-let max: number = 40;
+let max: number = 41;
 let personas: number;
 let menores: number = 0;
 let mayores: number = 0;
 //usar nombres de variables empezando con minuscula y ademas deben ser una accion ej: cargarEdadAleatoria
-function Edades(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min)) + min;//los de 40 entran???
+function cargarEdadesAzar(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min)) + min; //los de 40 entran???
 }
 
 for (personas = 0; personas < 270; personas++) {
-  if (Edades(min, max) < 21) {
+  if (cargarEdadesAzar(min, max) < 21) {
     menores += 1;
   } else mayores += 1;
 }
